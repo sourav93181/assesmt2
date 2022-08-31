d1ab73a15e7af3abaa0a50f10f0415345c01becb
sourav parmar <sourav93181@gmail.com>
2022-08-31 16:11:14 +0530

    added

---
+++ assessment2/store-app/src/redux/todoSlice.js
@@ -0,0 +1,83 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getBrowsingData } from "../api/browsingApi";
+import getDenimimg from "../api/clothesdenimapi/denim";
+import getRealmeimg from "../api/realmeapi/realme";
+
+const cardslice = createSlice({
+  name: "cards",
+  initialState: {
+    search: "",
+    sizeOfCart: 0,
+    listOfAllObject: getBrowsingData(),
+    listOfRealmeObject: getRealmeimg(),
+    listOfClothesObject: getDenimimg(),
+    listOfCartOjbect: [],
+    
+  },
+  reducers: {
+    onSearchChanging(state, action) {
+      state.search = action.payload;
+    },
+    incrementCart(state, action) {
+      state.sizeOfCart += 1;
+    },
+    decrementCart(state, action) {
+      state.sizeOfCart -= 1;
+    },
+    handleincrese1(state, action) {
+      const productexit = state.listOfAllObject.find(
+        (item) => item.id === action.payload
+      );
+
+      state.listOfAllObject = state.listOfAllObject.map((item) =>
+        item.id === action.payload
+          ? { ...productexit, quantity: productexit.quantity + 1 }
+          : item
+      );
+    },
+    handledecrese1(state, action) {
+      const productexit = state.listOfAllObject.find(
+        (item) => item.id === action.payload
+      );
+
+      state.listOfAllObject = state.listOfAllObject.map((item) =>
+        item.id === action.payload
+          ? {
+              ...productexit,
+              quantity:
+                productexit.quantity === 0
+                  ? productexit.quantity
+                  : productexit.quantity - 1,
+            }
+          : item
+      );
+    },
+
+    handleproduct2(state, action) {
+      const productexit = state.listOfCartOjbect.find(
+        (item) => item.id === action.payload.id
+      );
+      console.log(action.payload);
+      if (productexit) {
+        state.listOfCartOjbect = state.listOfCartOjbect.map((item) =>
+          item.id === action.payload.id ? { ...productexit } : item
+        );
+      } else {
+        state.listOfCartOjbect = [
+          ...state.listOfCartOjbect,
+          { ...action.payload },
+        ];
+      }
+    },
+  },
+});
+
+export const {
+  onSearchChanging,
+  incrementCart,
+  decrementCart,
+  handledecrese1,
+  handleincrese1,
+  handleproduct2,
+} = cardslice.actions;
+export default cardslice.reducer;
